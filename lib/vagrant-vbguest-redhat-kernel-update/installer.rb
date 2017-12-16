25916f1d5734fab53d5788b85ce26541769494de
Andrew Sharpe <asharpe@squiz.net>
2017-12-16 11:54:16 +1000

    get reboot working with CentOS7 guest

--- lib/vagrant-vbguest-redhat-kernel-update/installer.rb
+++ lib/vagrant-vbguest-redhat-kernel-update/installer.rb
@@ -20,7 +20,8 @@ module VagrantVbguestRedHatKernelUpdate
   protected
 
     def restart_additions(opts=nil, &block)
-      communicate.sudo("/etc/init.d/vboxadd restart", opts, &block)
+      # TODO appropriate restart with systemd
+      communicate.sudo("[[ -f /etc/init.d/vboxadd ]] && /etc/init.d/vboxadd restart || :", opts, &block)
     end
 
     # TODO submit MR to have this in upstream
@@ -53,9 +54,37 @@ module VagrantVbguestRedHatKernelUpdate
       @env.ui.warn("Attempting to upgrade the kernel")
       communicate.sudo("yum -y upgrade kernel{,-devel,-headers}", opts, &block)
       @env.ui.warn("Restarting to activate upgraded kernel")
-      reboot!(@vm, {:auto_reboot => true})
+
+      # should work from what I can tell, but doesn't :(
+      #vm.action(:reload, {});
+      reboot(@vm, {:auto_reboot => true})
+
+      # hide this reboot from vbguest
+      #@@rebooted[ self.class.vm_id(vm) ] = false
     end
 
+    # I have NFI why @vm.action(:reload) doesn't work!
+    # It's as though we can't hook into the action chain fully, and the result is
+    # there's a bogus check for the guest additions before the boot which causes
+    # failure
+    def reboot(vm, options)
+      simple_reboot = Vagrant::Action::Builder.new.tap do |b|
+        b.use Vagrant::Action::Builtin::Call, Vagrant::Action::Builtin::GracefulHalt, :poweroff, :running do |env2, b2|
+          if !env2[:result]
+            b2.use VagrantPlugins::ProviderVirtualBox::Action::ForcedHalt
+          end
+        end
+        b.use VagrantPlugins::ProviderVirtualBox::Action::Boot
+        if defined?(Vagrant::Action::Builtin::WaitForCommunicator)
+          b.use Vagrant::Action::Builtin::WaitForCommunicator, [:starting, :running]
+        end
+      end
+      # this is not ideal - we're just grabbing the bits we need to allow this to work
+      @env.action_runner().run(simple_reboot, {
+        :ui => @env.ui,
+        :machine => vm,
+      })
+    end
 
   end
 end
